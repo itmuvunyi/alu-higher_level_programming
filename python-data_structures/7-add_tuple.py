fb6422bf23c4446af7fe88da3a3e88bd4964be7c
itmuvunyi <i.muvunyi@alustudent.com>
2025-02-20 10:49:15 -0800

    a function that adds 2 tuples.

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     # Ensure both tuples have at least two elements
-    a1, a2 = (tuple_a + (0, 0))[:2]  # Fill with 0 if tuple_a is smaller than 2 elements
-    b1, b2 = (tuple_b + (0, 0))[:2]  # Fill with 0 if tuple_b is smaller than 2 elements
-    
+    a1, a2 = (tuple_a + (0, 0))[:2]
+    b1, b2 = (tuple_b + (0, 0))[:2]
     return (a1 + b1, a2 + b2)
-
