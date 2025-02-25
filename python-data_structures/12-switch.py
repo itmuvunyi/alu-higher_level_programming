7d11d70a441e747dafbbf4c4ffd3970736fcbcc9
itmuvunyi <i.muvunyi@alustudent.com>
2025-02-25 12:28:30 -0800

    the source code in order to switch value of a and b

--- python-data_structures/12-switch.py
+++ python-data_structures/12-switch.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-a = 1
-b = 2
 
-# Switch the values of a and b
-a, b = b, a
+def switch_values(a, b):
+    # Switch the values of a and b
+    a, b = b, a
+    return a, b
 
-print(a)
-print(b)
+a = 5
+b = 10
+print(switch_values(a, b))
