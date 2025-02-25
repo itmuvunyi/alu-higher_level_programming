4a28c2411c485bce0adff97325992d59d5284ec3
itmuvunyi <i.muvunyi@alustudent.com>
2025-02-25 12:04:53 -0800

    a function that prints an integer.

---
+++ python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_integer_err(value):
+    try:
+        # Try printing the value as an integer
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError) as e:
+        # If an error occurs, print it to stderr
+        import sys
+        print(f"Exception: {e}", file=sys.stderr)
+        return False
