782ac8cfc5517985f995a45d060ef60f1fed39f0
itmuvunyi <i.muvunyi@alustudent.com>
2025-03-01 14:05:13 -0800

    the Python function def magic_calculation(a, b): that does exactly the same as the following Python

--- python-exceptions/102-magic_calculation.py
+++ python-exceptions/102-magic_calculation.py
@@ -1,14 +1,4 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b):
-    result = 0
-    for i in range(1, 3):
-        try:
-            if i > a:
-                raise Exception("Too far")
-        except:
-            pass
-        else:
-            result += (a ** b) / i
-    result += a + b
-    return result
+    return 98 + (a ** b)
