bdfb633213678ffde7ec2924fc8a0fe8045a0b51
itmuvunyi <i.muvunyi@alustudent.com>
2025-03-01 14:10:23 -0800

    Updated  the Python function def magic_calculation(a, b): that does exactly

--- python-exceptions/102-magic_calculation.py
+++ python-exceptions/102-magic_calculation.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b):
-    return 98 + (a ** b)
+    result = 1
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception("Too far")
+            result *= a ** b / i
+        except Exception:
+            result = b + a
+            break
+    return result
+
