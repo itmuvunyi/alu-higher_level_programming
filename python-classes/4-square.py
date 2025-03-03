b8f200ee9ded1a28cd4a224eed1a2dddac794848
itmuvunyi <i.muvunyi@alustudent.com>
2025-03-03 14:04:38 -0800

    Updated a class Square that defines a square by: (based on 3-square.py)

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,32 +1,25 @@
 #!/usr/bin/python3
-
-
-"""This module defines a Square class with a private size attribute and getter/setter methods."""
+"""Write a class Square that defines a square"""
 
 
 class Square:
-    """Class that defines a square by its size."""
-    
+    """Write a class Square that defines a square"""
+
     def __init__(self, size=0):
-        """Initializes the square with a size."""
-        self.size = size  # Using the setter to set the initial size
-    
+        self.size = size
+
     @property
     def size(self):
-        """Getter for size."""
         return self.__size
-    
+
     @size.setter
     def size(self, value):
-        """Setter for size."""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-        
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
-        
-        self.__size = value  # Private attribute
-    
+        else:
+            self.__size = value
+
     def area(self):
-        """Returns the current square area."""
-        return self.__size * self.__size
+        return self.__size**2
