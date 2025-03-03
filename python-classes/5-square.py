0bbc79fc046431dfdfa2a6719503a19a1b0e041e
itmuvunyi <i.muvunyi@alustudent.com>
2025-03-03 14:07:06 -0800

    Updated a class Square that defines a square by: (based on 4-square.py)

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,40 +1,34 @@
 #!/usr/bin/python3
-
-
-"""This module defines a Square class with a private size attribute, area method, and my_print method."""
+"""Write a class Square that defines a square by"""
 
 
 class Square:
-    """Class that defines a square by its size."""
-    
+    """Write a class Square that defines a square by"""
+
     def __init__(self, size=0):
-        """Initializes the square with a size."""
-        self.size = size  # Using the setter to set the initial size
-    
+        self.size = size
+
     @property
     def size(self):
-        """Getter for size."""
         return self.__size
-    
+
     @size.setter
     def size(self, value):
-        """Setter for size."""
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        
-        if value < 0:
+        if type(value) is not int:
+            raise TypeError("size must be integer")
+        elif value < 0:
             raise ValueError("size must be >= 0")
-        
-        self.__size = value  # Private attribute
-    
+        else:
+            self.__size = value
+
     def area(self):
-        """Returns the current square area."""
-        return self.__size * self.__size
-    
+        return self.__size**2
+
     def my_print(self):
-        """Prints the square using the character #."""
         if self.__size == 0:
             print()
         else:
-            for _ in range(self.__size):
-                print("#" * self.__size)
+            for i in range(self.__size):
+                for s in range(self.__size):
+                    print('#', end="")
+                print()
