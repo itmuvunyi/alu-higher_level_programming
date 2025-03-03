d19d0fecb02641d37644db24a5f6578f7fd94a05
itmuvunyi <i.muvunyi@alustudent.com>
2025-03-03 14:02:32 -0800

    Updated a class Square that defines a square by: (based on 2-square.py)

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-"""This module defines a Square class with a private size attribute and an area method."""
+"""Write a class Square that defines a square"""
+
+
 class Square:
-    """Class that defines a square by its size."""
+    """Write a class Square that defines a square"""
+
     def __init__(self, size=0):
-        """Initializes the square with a size."""
-        if not isinstance(size, int):
+        if type(size) is not int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size  # Private attribute
+        else:
+            self.__size = size
+
     def area(self):
-        """Returns the current square area."""
-        return self.__size * self.__size
+        return self.__size**2
