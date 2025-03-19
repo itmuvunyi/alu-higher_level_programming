7aa10f253cac2376c0403469d8a0dbcc6bde5e97
itmuvunyi <i.muvunyi@alustudent.com>
2025-03-19 16:40:13 -0700

    All network1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -4,7 +4,7 @@
 import urllib.request
 
 if __name__ == "__main__":
-    url = "http://0.0.0.0:5050/status"
+    url = "https://intranet.hbtn.io/status"
 
     with urllib.request.urlopen(url) as response:
         html = response.read()
