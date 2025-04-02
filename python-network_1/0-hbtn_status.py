29a4ec8f09af54b53415a023a8852003cdbce6ff
itmuvunyi <i.muvunyi@alustudent.com>
2025-04-02 11:17:31 +0000

    update

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,11 +2,20 @@
 """ Python script that fetches https://intranet.hbtn.io/status """
 import urllib.request
 
-
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
+    url = 'https://intranet.hbtn.io/status'
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
+    
+    request = urllib.request.Request(url, headers=headers)
+    
+    try:
+        with urllib.request.urlopen(request) as response:
+            html = response.read()
+            print('Body response:')
+            print('\t- type: {}'.format(type(html)))
+            print('\t- content: {}'.format(html))
+            print('\t- utf8 content: {}'.format(html.decode("utf-8")))
+    except urllib.error.HTTPError as e:
+        print(f"HTTP Error: {e.code} - {e.reason}")
+    except urllib.error.URLError as e:
+        print(f"URL Error: {e.reason}")
