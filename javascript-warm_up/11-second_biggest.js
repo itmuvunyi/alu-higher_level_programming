173aece18fdfbfe184a58a47bf2a9685da164b77
itmuvunyi <i.muvunyi@alustudent.com>
2025-05-21 04:15:31 -0700

    some of all

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const args = process.argv.slice(2).map(Number);
+
+if (args.length < 2) {
+  console.log(0);
+} else {
+  const max = Math.max(...args);
+  const withoutMax = args.filter(num => num !== max);
+  const secondMax = Math.max(...withoutMax);
+  console.log(secondMax);
+}
