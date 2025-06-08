713a151a592c249a2d603cbce617ded19be4c15d
itmuvunyi <i.muvunyi@alustudent.com>
2025-06-08 02:59:12 -0700

    All jquery tasks

---
+++ javascript-web_jquery/4-script.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+$('#toggle_header').click(function() {
+  const header = $('header');
+  if (header.hasClass('red')) {
+    header.removeClass('red').addClass('green');
+  } else {
+    header.removeClass('green').addClass('red');
+  }
+});
